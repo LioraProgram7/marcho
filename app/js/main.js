3414e91e65f7a7f75dd0fb8a1cee2a1070368e8f
LioraProgram7 <sv.kiselis@gmail.com>
2022-02-03 20:08:23 +0200

    added top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
 
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,   // плавный переход
+    autoplay: true,   // слайдер меняется автоматически
+    autoplaySpeed: 2000 
+    // каждых 2000мс (2 минуты) слайд меняется
+    
+  });
+
 });
