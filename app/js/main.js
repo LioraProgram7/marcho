8d12434a220977c1555c6d1c4978d32f60c5f916
LioraProgram7 <sv.kiselis@gmail.com>
2022-02-07 14:05:09 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,19 @@ $(function () {
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
-    fade: true,   // плавный переход
-    autoplay: true,   // слайдер меняется автоматически
-    autoplaySpeed: 2000 
+    fade: true, // плавный переход
+    autoplay: true, // слайдер меняется автоматически
+    autoplaySpeed: 2000
     // каждых 2000мс (2 минуты) слайд меняется
-    
+
+  });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+
   });
 
 });
